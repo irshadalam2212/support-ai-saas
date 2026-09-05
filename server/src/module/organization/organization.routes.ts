74153f7752604c382be525c38356a5f81faecbd5
irshad <irshadalam1414@gmail.com>
2026-09-06 02:20:52 +0530

    database table changes

--- server/src/module/organization/organization.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createOrganization } from "./organization.controller";
-import { authMiddleware } from "../../middlewares/auth.middleware";
-
-const router = Router();
-
-router.post("/", authMiddleware, createOrganization);
-
-export default router;
